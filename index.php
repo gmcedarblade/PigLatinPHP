a349a88e7071327fada6183c3e8f2f9af75b45c4
Greg Cedarblade <gmcedarblade@gmail.com>
2017-04-18 16:26:52 -0500

    can translate into pig latin

--- index.php
+++ index.php
@@ -35,27 +35,56 @@ FORM;
                 
 				
                 // Get the input string and check to see if it contains characters
-				// break the input string up into individual words
-				// for each individual word, perform the following tasks
-				
-					// Strip out all non-letter characters - hint (".,;:'\"? \t\n-()_0..9")
-					// Check to see if the word has any character's remaining
-					// if the word still has characters perform the following tasks
-					
-						// if first letter is a vowel, leave it in place
-                        // and append "way" to the end of the word
-                        // else move the first letter to the end of the word
-                        // and append "ay"
-				
-						// append the newly created pig Latin word to an output line
-					
-					// output translated pig Latin phrase in the following output line	
+
+                $inputString = $_GET['inputString'];
+
+                if($inputString) {
+
+                    // break the input string up into individual words
+                    $words = explode(" ", $inputString);
+                    $newPhrase = "";
+
+                    // for each individual word, perform the following tasks
+                    foreach($words as $word) {
+
+                        // Strip out all non-letter characters - hint (".,;:'\"? \t\n-()_0..9")
+                        $word = trim($word, ".,;:'\"? \t\n-()_0..9");
+
+                        // Check to see if the word has any character's remaining
+                        if($word) {
+
+                            // If the word still has characters perform the following tasks:
+
+                            // if first letter is a vowel, leave it in place
+                            if(stristr("aeiou", substr($word, 0 ,1))) {
+
+                                // and append "way" to the end of the word
+                                $word .= "way";
+
+                            } else {
+
+                                // else move the first letter to the end of the word
+                                // and append "ay"
+                                $firstLetter = substr($word, 0, 1);
+                                $word = substr($word, 1) . $firstLetter . "ay";
+
+                            }
+
+                            // append the newly created pig Latin word to an output line
+                            $newPhrase .= $word . " ";
+
+                        }
+                    }
+
+
+                    // output translated pig Latin phrase in the following output line
                     echo "<h3 style=\"color:yellow;background-color:navy\">$inputString "
-                            . "translated into Pig Latin: $newPhrase</h3>\n";
+                        . "translated into Pig Latin: $newPhrase</h3>\n";
+                } else {
 
                     // If the input string was initially blank output the following line
                     echo "<h3 style=\"color:yellow;background-color:navy\">Why you no enter text?</h3>";
-                
+                }
             } // end if form has been submitted
         ?>
     </body>
